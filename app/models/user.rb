078a69c43ceb17327ae84736b16a40fdeacbcac4
Artem Kharshan <timkharshan@hotmail.com>
2017-11-25 10:40:20 -0800

    evetnlist styling, fixed password validation lol

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 
 class User < ApplicationRecord
     validates :username, :session_token, presence: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
     validates :password_digest, length:{minimum: 6, allow_nil:true }
     validates :username, uniqueness: true
 
