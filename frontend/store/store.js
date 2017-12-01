69b3c3b40bf26d3ae9af4de8e09b485c93080003
Artem Kharshan <timkharshan@hotmail.com>
2017-12-01 10:17:25 -0800

    styling, about to fix authorship

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,15 +1,24 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+
 
 import rootReducer from '../reducers/root_reducer';
 
-const configureStore = (preloadedState = {}) => (
+const configureStore = (preloadedState = {}) => 
+{
+    const middlewares = [thunk];
+
+    if (process.env.NODE_ENV !== 'production') {
+        // must use 'require' (import only allowed at top of file)
+        const { logger } = require('redux-logger');
+        middlewares.push(logger);
+    }   
+    return(
     createStore(
         rootReducer,
         preloadedState,
-        applyMiddleware(thunk, logger)
+        applyMiddleware(...middlewares)
     )
 );
-
+}
 export default configureStore;
