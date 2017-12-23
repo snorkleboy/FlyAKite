808de8e5e2aace6c62419afc605285239ba97319
Tim Kharshan <timkharshan@hotmail.com>
2017-12-23 15:49:51 -0800

    maps loading from city and state

--- frontend/components/events/show_page_components.jsx
+++ frontend/components/events/show_page_components.jsx
@@ -24,18 +24,6 @@ export const EventHeader = ({date, header, name}) => {
     );
 };
 
-
-
-const mapCenter = { lat: 37.7758, lng: -122.435 };
-
-// I made some lat/lng points for some good burrito spots
-const burritos = [
-  { lat: 37.775785, lng: -122.445979, name: "Papalote" },
-  { lat: 37.772045, lng: -122.437015, name: "The Little Chihuahua" },
-  { lat: 37.781899, lng: -122.410426, name: "Cancun" }
-];
-
-
 export const EventDiscription = ({event}) => {
     return (
         <div className='description'>
@@ -54,13 +42,13 @@ export const EventDiscription = ({event}) => {
                     <span>{event.endDate? event.endDate.slice(0, 10) : ''}</span> 
                 </div>
                 <div className='show-location'><h1>LOCATION</h1>
-                    <span>{event.city}</span>
+                    <span>{event.location.city}</span>
                     <br></br>
-                    <span>{event.areaCode}</span>
+                    <span>{event.location.areaCode}</span>
                     <br></br>
-                    <span>{event.state}</span>
+                    <span>{event.location.state}</span>
                 </div>
-                <Map center={mapCenter} burritoPlaces={burritos} />
+                <Map location={{ areaCode: event.location.areaCode, city: event.location.city, state: event.location.state }}/>
             </div>
         </div>
         
