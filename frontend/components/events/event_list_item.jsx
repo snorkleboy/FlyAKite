705fb08ebff19c1791064f619544b2516cfadd85
Tim Kharshan <timkharshan@hotmail.com>
2017-12-23 21:33:52 -0800

    fixes and styles

--- frontend/components/events/event_list_item.jsx
+++ frontend/components/events/event_list_item.jsx
@@ -7,21 +7,23 @@ const EventListItem = ({ event, registrationHandler, registered, bookmarkHandler
     return(
         <li className='event-list-item-li'> 
             <div className='event-image-container'>
-            <Link to={`/events/${event.id}`} >
-                <img className='event-list-item-image-c' src={imgURL} />
-            
-            </Link>
+                <Link to={`/events/${event.id}`} >
+                    <img className='event-list-item-image-c' src={imgURL} />
+                </Link>
             </div> 
-            <span className='image-buttons-holder'>
-                <button className={(bookmarked ? 'un' : '') + 'bookmark-button'} onClick={bookmarkHandler}><i className={"fa fa-bookmark" + (bookmarked ? '-o' : '')}></i>bookmark{bookmarked ? 'ed' : ''}  </button>
-            </span>
+            <div  className='BOOKMARK'>
+                <button
+                    className={(bookmarked ? 'un' : '') + 'bookmark-button' + ' floatbutton'}
+                    onClick={bookmarkHandler}
+                >
+                    <i className={"fa fa-bookmark" + (!bookmarked ? '-o' : '')}></i>
+                </button>
+            </div>
             <div className='event-list-item-header-c'>
+
                 <span className='header-date-el'>{event.startDate.slice(0, 10)}</span> 
                 <span className='header-name-el'>{event.name}</span> 
                 <span className='header-location-el'>{event.location.city}</span>
-
-                
-
             </div>
         </li>
     );
