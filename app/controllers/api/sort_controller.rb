e69e30044bc441a0c88358dd2174fa32de5c3bb2
Artem Kharshan <timkharshan@hotmail.com>
2018-02-05 16:14:22 -0800

    added engines to package json

--- app/controllers/api/sort_controller.rb
+++ app/controllers/api/sort_controller.rb
@@ -10,13 +10,6 @@ class Api::SortController < ApplicationController
         indexRender
     end
     def search
-        # if (params[:categoryId].to_i>0)
-        #     @events = Event.find_by_category_and_string(params[:categoryId],params[:pattern])
-        #     .limit(params[:limit] || 10).offset(params[:offset] || 0)
-        # else
-        #     @events = Event.find_by_string(params[:pattern])
-        #     .limit(params[:limit] || 10).offset(params[:offset] || 0)
-        # end
         @events = Event.search(params)
         indexRender
     end
