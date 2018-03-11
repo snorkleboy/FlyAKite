a0978ffd2ace2eb929f6d8e2429b077327d1d9c9
Artem Kharshan <timkharshan@hotmail.com>
2018-03-11 13:35:07 -0700

    free registration fixed

--- frontend/util/registrationApi.js
+++ frontend/util/registrationApi.js
@@ -1,8 +1,9 @@
 export const createRegistration = (eventId, userId, stripeDet) => $.ajax({
-    url: `api/registration/${eventId}/${userId}`,
-    method: "POST",
-    data:{stripeKey: stripeDet.id, stripeEmail: stripeDet.email}
-});
+        url: `api/registration/${eventId}/${userId}`,
+        method: "POST",
+        data:stripeDet ? {stripeKey: stripeDet.id, stripeEmail: stripeDet.email} : null
+    });
+
 
 export const deleteRegistration = (eventId) => $.ajax({
     url: `api/registration/${eventId}`,
